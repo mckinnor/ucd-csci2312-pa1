81fee098f8fabde017056fe47ebc11c00c5f8b7d
mckinnor <ryanmckinnon97@gmail.com>
2016-02-03 15:12:37 -0700

    Fixing errors.

--- Point.cpp
+++ Point.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 double Point::distanceTo(const Point &p1, const Point &p2) const {
-    int x1, x2, y1, y2, z1, z2;
+    double x1, x2, y1, y2, z1, z2;
     double dis;
 
     x1 = p1.getX();
@@ -19,7 +19,7 @@ double Point::distanceTo(const Point &p1, const Point &p2) const {
     y2 = p2.getY();
     z2 = p2.getZ();
 
-    dis = sqrt(pow((z2 - z2),2) + pow((x2 - x1),2) + pow((y2 - y2),2));
+    dis = sqrt(pow((z2 - z1),2) + pow((x2 - x1),2) + pow((y2 - y1),2));
 
     return dis;
 }
--- area.cpp
+++ area.cpp
@@ -2,17 +2,20 @@
 // Created by Ryan on 2/3/2016.
 //
 #include <cmath>
+#include "Point.h"
 
 using namespace std;
 
-double computeArea(const Point &a, const Point &b, const Point &b) {
+double computeArea(const Point &a, const Point &b, const Point &c) {
     double side1, side2, side3, semiPerm, area;
 
-    side1 = distanceTo(a, b);
-    side2 = distanceTo(b, c);
-    side3 = distanceTo(c, a);
+    side1 = a.distanceTo(a, b);
+    side2 = b.distanceTo(b, c);
+    side3 = c.distanceTo(c, a);
 
     semiPerm = (side1 + side2 + side3)/2;
 
     area = sqrt(semiPerm*(semiPerm - side1)*(semiPerm - side2)*(semiPerm - side3));
+
+    return area;
 }
