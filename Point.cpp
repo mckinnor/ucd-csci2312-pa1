1a1c2b30b0e7e14834c2b8ce26a64798750b06b2
mckinnor <ryanmckinnon97@gmail.com>
2016-02-03 14:33:34 -0700

    Updated getters/setters and the distanceTo functions to work in 3d space.

--- Point.cpp
+++ Point.cpp
@@ -8,16 +8,18 @@
 using namespace std;
 
 double Point::distanceTo(const Point &p1, const Point &p2) const {
-    int x1, x2, y1, y2;
+    int x1, x2, y1, y2, z1, z2;
     double dis;
 
     x1 = p1.getX();
     y1 = p1.getY();
+    z1 = p1.getZ();
 
     x2 = p2.getX();
     y2 = p2.getY();
+    z2 = p2.getZ();
 
-    dis = sqrt(pow((x2 - x1),2) + pow((y2 - y2),2));
+    dis = sqrt(pow((z2 - z2),2) + pow((x2 - x1),2) + pow((y2 - y2),2));
 
     return dis;
 }
--- Point.h
+++ Point.h
@@ -7,21 +7,22 @@
 class Point {
     
 private:
-    double x, y;
+    double x, y, z;
 
 public:
     // Constructors
     Point();                      // default constructor
-    Point(double x, double y);    // two-argument constructor
+    Point(double x, double y, double z);    // two-argument constructor
 
     // Mutator methods
     void setX(double newX){x = newX;}
     void setY(double newY){y = newY;}
+    void setZ(double newZ){z = newZ;}
 
     // Accessor methods
     double getX() const{return x;}
     double getY() const{return y;}
-
+    double getZ() const{return z;}
 
     double distanceTo(const Point &p1, const Point &p2) const;
 };
